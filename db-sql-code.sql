2715c40026db3992c2c62cd07298d96b3fd7ac91
collinshoromba <collinserver001@gmail.com>
2024-01-21 02:07:55 +0200

    commit this

--- db-sql-code.sql
+++ db-sql-code.sql
@@ -238,4 +238,9 @@ VALUES   (
     5
   );
 
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
+
 
