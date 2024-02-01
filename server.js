5ba298796eef92ac6fe7753d98c0e86f20778123
collinshoromba <collinserver001@gmail.com>
2024-02-01 17:54:42 +0200

    commit this

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/InventoryRoute")
 
 
 /* ***********************
@@ -24,9 +25,11 @@ app.set("layout", "./layouts/layout")
  * Routes
  *************************/
 app.use(static)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 //index route
-app.get("/", function(req, res){res.render("index", {title: "Home"})})
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
